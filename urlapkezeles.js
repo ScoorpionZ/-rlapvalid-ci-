c45b7b584754a4fb24586e0ad864f0e0096558af
ScoorponZ <varadi.akos14@gmail.com>
2021-04-14 14:44:10 +0200

    urlapelemek

---
+++ urlapkezeles.js
@@ -0,0 +1,71 @@
+window.addEventListener("load",init);
+
+function ID(nev) {
+    return document.getElementById(nev);
+}
+
+function $(nev) {
+    return document.querySelectorAll(nev);
+}
+
+function init(){
+    
+    ID("kuld").addEventListener("click",valialas);
+}
+
+function valialas(){
+    //console.log("Hello");
+    var uzenet = "";
+    var magadottAdatok="";
+    if(ID("nev").value.length<3){
+        uzenet = "A név legalább 3 karakter hosszú legyen! <br>";
+        ID("nev").style.border="2px solid red";
+    }
+    else{
+        ID("nev").style.border="none";
+    }
+
+    var reg = /[A-Z]+[a-z]{2,}/;
+    if (!reg.test(ID("nev").value)) {
+        uzenet += "A név legalább 3 karakter hosszú legyen! A név nagybetűvel kell kezdődjön! <br>";
+        ID("nev").style.border="2px solid red";
+    }
+    else{
+        ID("nev").style.border="none";
+        magadottAdatok += "Név: "+ ID("nev").value+"< br>";
+    }
+   // console.log(uzenet);
+
+   var emailreg = /[a-z]+\@[a-z]+\.hu$/;
+    if (!emailreg.test(ID("email1").value)) {
+        uzenet += "Az emeil taralmazzon @ jelet és .hu-ra végződjön <br>";
+        ID("email1").style.border="2px solid red";
+    }
+    else{
+        ID("email1").style.border="none";
+        magadottAdatok += "Név: "+ ID("email1").value+"<br>";
+    }
+    if ( !emailreg.test(ID("email2").value) ) {
+        uzenet += "Az emeil taralmazzon @ jelet és .hu-ra végződjön <br>";
+        ID("email2").style.border="2px solid red";
+    }
+    else{
+        ID("email2").style.border="none";
+        magadottAdatok += "Név: "+ ID("email2").value+"<br>";
+    }
+
+    var telreg = /^\+[0-9]{11}$/;
+    if ( !telreg.test(ID("phone").value) ) {
+        uzenet += "Az telefonszám csak számokat tartalmazhat <br>";
+        ID("phone").style.border="2px solid red";
+    }
+    else{
+        ID("phone").style.border="none";
+        magadottAdatok += "Név: "+ ID("phone").value+"<br>";
+    }
+   
+   
+
+   $("aside>section:nth-child(1)>p")[0].innerHTML=uzenet;
+   $("aside>section:nth-child(2)>p")[0].innerHTML = magadottAdatok;
+}
